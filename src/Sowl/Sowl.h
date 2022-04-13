0aade56e7ed0453e0e0d2bb529283a6cc7d3eaf6
Otto Fernando Pflücker López <otto.pflucker@external.wolterskluwer.com>
2022-04-13 23:10:21 +0200

    Completed includes in Sowl.h.

--- src/Sowl/Sowl.h
+++ src/Sowl/Sowl.h
@@ -5,13 +5,29 @@
 #endif 
 
 #include <Windows.h>
+
 #include "Windows/MessageLoop.h"
+#include "Windows/Utilities.h"
+#include "Windows/Window.h"
 #include "Windows/WindowHandleCreator.h"
-#include "Windows/Customs/WindowClassRegisterer.h"
-#include "Windows/Customs/CustomWindow.h"
+
+#include "Windows/Controls/ControlCreator.h"
+#include "Windows/Controls/ControlWindow.h"
 #include "Windows/Controls/ButtonWindow.h"
+
+#include "Windows/Customs/CustomWindow.h"
+#include "Windows/Customs/WindowClassRegisterer.h"
+
+#include "Windows/Dialogs/DialogWindow.h"
 #include "Windows/Dialogs/ModalDialogWindow.h"
 #include "Windows/Dialogs/ModelessDialogWindow.h"
+
+#include "Graphics/DeviceContext.h"
+#include "Graphics/PaintDeviceContext.h"
+
+#include "Graphics/Brushes/Brush.h"
 #include "Graphics/Brushes/SolidBrush.h"
-#include "Graphics/Cursors/PredefinedCursorHandles.h"
 #include "Graphics/Brushes/StockBrushesHandles.h"
+
+#include "Graphics/Cursors/Cursor.h"
+#include "Graphics/Cursors/PredefinedCursorHandles.h"
